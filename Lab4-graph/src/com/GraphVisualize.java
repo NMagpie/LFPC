a9a6570a3c1ec7d2695749f9cc0ae550f4c406ca
NMagpie <sorochin.work@gmail.com>
2021-05-14 04:20:09 +0300

    Changed Lab4.

--- Lab4-graph/src/com/GraphVisualize.java
+++ Lab4-graph/src/com/GraphVisualize.java
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 package com;
 
 import org.graphstream.graph.Graph;
@@ -46,3 +48,103 @@ public class GraphVisualize implements ViewerListener {
     public void mouseLeft(String s) {
     }
 }
+=======
+package com;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerListener;
+import org.graphstream.ui.view.ViewerPipe;
+
+//this class just needs to display the graph on the screen and then wait until the window with the graph is closed
+//after that program starts to parse next string, if there is one.
+
+public class GraphVisualize implements ViewerListener {
+    private boolean loop;
+    private ViewerPipe pipe;
+
+    public void show(Graph graph) {
+        loop=true;
+        Viewer viewer = graph.display();
+        pipe = viewer.newViewerPipe();
+        pipe.addViewerListener(this);
+        pipe.addAttributeSink(graph);
+        viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.CLOSE_VIEWER);
+
+        while (loop) {
+            pipe.pump(); }
+    }
+
+    @Override
+    public void viewClosed(String s) {
+        loop=false;
+        pipe.clearSinks();
+    }
+
+    @Override
+    public void buttonPushed(String s) {
+    }
+
+    @Override
+    public void buttonReleased(String s) {
+    }
+
+    @Override
+    public void mouseOver(String s) {
+    }
+
+    @Override
+    public void mouseLeft(String s) {
+    }
+}
+>>>>>>> cef7ed740079ebdf612c16e6fbc9ac7a7782fef4
+=======
+package com;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerListener;
+import org.graphstream.ui.view.ViewerPipe;
+
+//this class just needs to display the graph on the screen and then wait until the window with the graph is closed
+//after that program starts to parse next string, if there is one.
+
+public class GraphVisualize implements ViewerListener {
+    private boolean loop;
+    private ViewerPipe pipe;
+
+    public void show(Graph graph) {
+        loop=true;
+        Viewer viewer = graph.display();
+        pipe = viewer.newViewerPipe();
+        pipe.addViewerListener(this);
+        pipe.addAttributeSink(graph);
+        viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.CLOSE_VIEWER);
+
+        while (loop) {
+            pipe.pump(); }
+    }
+
+    @Override
+    public void viewClosed(String s) {
+        loop=false;
+        pipe.clearSinks();
+    }
+
+    @Override
+    public void buttonPushed(String s) {
+    }
+
+    @Override
+    public void buttonReleased(String s) {
+    }
+
+    @Override
+    public void mouseOver(String s) {
+    }
+
+    @Override
+    public void mouseLeft(String s) {
+    }
+}
+>>>>>>> cef7ed740079ebdf612c16e6fbc9ac7a7782fef4
--- Lab4-graph/src/com/Main.java
+++ Lab4-graph/src/com/Main.java
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 package com;
 
 public class Main {
@@ -6,4 +8,24 @@ public class Main {
         System.setProperty("org.graphstream.ui", "swing");
         SimplePrecedence.main();
     }
+=======
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui", "swing");
+        SimplePrecedence.main();
+    }
+>>>>>>> cef7ed740079ebdf612c16e6fbc9ac7a7782fef4
+=======
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui", "swing");
+        SimplePrecedence.main();
+    }
+>>>>>>> cef7ed740079ebdf612c16e6fbc9ac7a7782fef4
 }
