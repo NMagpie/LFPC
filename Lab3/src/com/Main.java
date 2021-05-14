334153c9fb12b8e8415edccc775769cbe308b6b4
NMagpie <sorochin.work@gmail.com>
2021-05-14 04:14:48 +0300

    Changed Lab3.

--- Lab3/src/com/Main.java
+++ Lab3/src/com/Main.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package com;
 
 public class Main {
@@ -19,3 +20,14 @@ public class Main {
     }
 }
 >>>>>>> 69c17f4c21e12b35b96f1b805eb6037471e09a37
+=======
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        //all the information about the algorithm and comments are located in this little method
+        //42
+        Parsing.parse();
+    }
+}
+>>>>>>> cef7ed740079ebdf612c16e6fbc9ac7a7782fef4
