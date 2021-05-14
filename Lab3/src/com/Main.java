20b00700173767069da158aae48c2d0b276c67b5
NMagpie <sorochin.work@gmail.com>
2021-05-14 04:21:58 +0300

    Lab3.

--- Lab3/src/com/Main.java
+++ Lab3/src/com/Main.java
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 package com;
 
 public class Main {
@@ -20,6 +21,17 @@ public class Main {
     }
 }
 >>>>>>> 69c17f4c21e12b35b96f1b805eb6037471e09a37
+=======
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        //all the information about the algorithm and comments are located in this little method
+        //42
+        Parsing.parse();
+    }
+}
+>>>>>>> cef7ed740079ebdf612c16e6fbc9ac7a7782fef4
 =======
 package com;
 
