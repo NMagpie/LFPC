0edba762e6b8a40576a6acc2943cbaa3ae5e145e
NMagpie <sorochin.work@gmail.com>
2021-05-14 04:22:15 +0300

    Lab4.

--- Lab4-graph/src/com/Main.java
+++ Lab4-graph/src/com/Main.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package com;
 
 public class Main {
@@ -7,6 +8,16 @@ public class Main {
         System.setProperty("org.graphstream.ui", "swing");
         SimplePrecedence.main();
     }
+=======
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui", "swing");
+        SimplePrecedence.main();
+    }
+>>>>>>> cef7ed740079ebdf612c16e6fbc9ac7a7782fef4
 =======
 package com;
 
