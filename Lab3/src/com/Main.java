aa1184091f4ec303056f9991fec27458eae4717b
NMagpie <sorochin.work@gmail.com>
2021-05-14 04:45:00 +0300

    commit

--- Lab3/src/com/Main.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class Main {
-    public static void main(String[] args) {
-        //all the information about the algorithm and comments are located in this little method
-        //42
-        Parsing.parse();
-    }
-}
